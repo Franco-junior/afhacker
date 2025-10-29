4af958241b992285b0d32380e2eeb5fcd4e341bc
Franco-junior <andersonsfj@al.insper.edu.br>
2025-10-29 02:12:22 -0300

    feat: Add security scan workflow and initialize package metadata

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+WebSecScanner - Security Vulnerability Scanner
+Main package initialization
+"""
+
+__version__ = "1.0.0"
+__author__ = "Franco"
+__description__ = "Advanced Web Application Security Scanner"
